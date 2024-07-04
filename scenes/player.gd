57307a04a78b0bceaf486cccfda4da4dbff71f11
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-07-04 17:41:47 +0300

    apply run animation to the player

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,6 +5,7 @@ const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta): 
@@ -16,9 +17,19 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	var direction = Input.get_axis("left", "right")
+	
 	if direction:
 		velocity.x = direction * SPEED
+		if direction<0:
+			animated_sprite.flip_h = true
+		else: 
+			animated_sprite.flip_h = false
+		animated_sprite.play("run")		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x == 0 :
+			animated_sprite.play("idle")
 
 	move_and_slide()
+			
+			
