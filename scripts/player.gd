6468a8ec26dc53a9d0074ad8fea5a1fc21a80869
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-07-10 18:01:34 +0300

    Apply a double jump when the condition is met.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,31 +3,46 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const MAX_JUMP = 2
 
+var can_double_jump = false
+var jumps = 0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta): 
-	
+	print(jumps)
 	if not is_on_floor():
-		velocity.y += gravity * delta
-
+		velocity.y += gravity * delta 
+		if Input.is_action_just_pressed("jump") and jumps > 0 and jumps < MAX_JUMP and can_double_jump:
+			jumps+=1	
+			velocity.y = JUMP_VELOCITY
+			animated_sprite.play("double_jump")
+	else:
+		jumps = 0
+				
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+		animated_sprite.play("jump")
+		velocity.y = JUMP_VELOCITY		
+		jumps+=1
+			
 	var direction = Input.get_axis("left", "right")
 	
+	# set direction left or right
+	if direction<0:
+			animated_sprite.flip_h = true
+	elif direction>0: 
+			animated_sprite.flip_h = false
+	
+	# Apply animation when player is on floor	run/idle
 	if direction:
 		velocity.x = direction * SPEED
-		if direction<0:
-			animated_sprite.flip_h = true
-		else: 
-			animated_sprite.flip_h = false
-		animated_sprite.play("run")		
+		if is_on_floor() and !Input.is_action_just_pressed("jump"):
+			animated_sprite.play("run")		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.x == 0 :
+		if is_on_floor() and !Input.is_action_just_pressed("jump"):
 			animated_sprite.play("idle")
 
 	move_and_slide()
